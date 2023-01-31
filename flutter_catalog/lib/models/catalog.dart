741d85316d4661f08a784fe702b725af76f5068e
officialhemant511 <officialhemant511@gmail.com>
2023-01-31 11:39:49 +0530

    Day 10 complete

--- flutter_catalog/lib/models/catalog.dart
+++ flutter_catalog/lib/models/catalog.dart
@@ -8,13 +8,3 @@ class Item {
 
   Item(this.id, this.name, this.desc, this.price, this.color, this.image);
 }
-
-final products = [
-  Item(
-    "Hackerrank", 
-    "iphone 13", 
-    "Apple iphone 12th generation", 
-     999, 
-    "#33505a",
-    "https://m.media-amazon.com/images/I/71E5zB1qbIL._SL1500_.jpg");
-];
