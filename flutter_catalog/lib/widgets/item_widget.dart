fe663c1cdecd18beee464dec927bca3b1785242b
officialhemant511 <officialhemant511@gmail.com>
2023-01-31 11:41:58 +0530

    complete

--- flutter_catalog/lib/widgets/item_widget.dart
+++ flutter_catalog/lib/widgets/item_widget.dart
@@ -10,7 +10,7 @@ class itemwidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8  .0),
+      padding: const EdgeInsets.all(8.0),
       child: Card(
         child: ListTile(
           onTap: () {
