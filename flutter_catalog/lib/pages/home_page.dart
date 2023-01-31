60cfad2b61d5380b6c5a0041c6a70080393f579b
officialhemant511 <officialhemant511@gmail.com>
2023-01-31 18:57:45 +0530

    Day 11 completed

--- flutter_catalog/lib/pages/home_page.dart
+++ flutter_catalog/lib/pages/home_page.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
 import '../models/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "hello world";
-  var dy = "monday"; // generic data type
+
+  var dy = "monday";
+  // generic data type
+
+  @override
+  void initState() {
+    // method to do initial state like here we put some product(items)
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  // load data is a user defined function
+  loadData() async {
+    // this is now we put json file in flutter
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    // here catalogjson will return a string formate output...
+    // print(catalogJson);// to check weather it will give correct output or not...
+    final decodedData = jsonDecode(catalogJson);
+    //print(decodeData);// do hot restart for check
+    //gives a map which is dynamic while decoding it will change string(catalogson) to map(decodedata)
+    //vice versa in jsonEncode();--> map to string in encoded form
+    var productsData = decodedData["products"];
+    // print(productsData);// check weather rather we get a productdata(item data) or not...
+    
+  }
 
   @override
   Widget build(BuildContext context) {
